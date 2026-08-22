d1250c461681abd08ab34a91931846f35d21edd9
Waldir Ampudia Cervantes <julinho19980105@gmail.com>
2026-08-22 13:00:28 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/shipping/page.tsx
+++ src/app/shipping/page.tsx
@@ -25,6 +25,7 @@ import { Calendar } from "@/components/ui/calendar"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Badge } from "@/components/ui/badge"
 import {
   Popover,
   PopoverContent,
